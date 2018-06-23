6757e05cfdc9e723e5e002982ab8d44abde8a7da
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-06-23 17:42:59 +0200

    try travis again w/o sudo

--- install-ompi3.sh
+++ install-ompi3.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -ex
-ls
-pwd
+cd ..
 mkdir openmpi3
 cd openmpi3
+OMP_DIR=$(pwd)
 wget --no-check-certificate https://download.open-mpi.org/release/open-mpi/v3.0/openmpi-3.0.0.tar.gz
 tar -zxf openmpi-3.0.0.tar.gz
 cd openmpi-3.0.0
-./configure --prefix=/usr/openmpi3
-sudo make all install
-echo "done with install"
-cd ..
-ls
-rm -rf openmpi3
+./configure --prefix=$OMP_DIR
+make all install
+echo "done with installing openMPI"
