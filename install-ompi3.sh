78c41ba54dd7f1f3c3f0abbf216ebd65a2f85731
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-06-23 18:20:14 +0200

    change ompi install path

--- install-ompi3.sh
+++ install-ompi3.sh
@@ -8,5 +8,5 @@ wget --no-check-certificate https://download.open-mpi.org/release/open-mpi/v3.0/
 tar -zxf openmpi-3.0.0.tar.gz
 cd openmpi-3.0.0
 ./configure --prefix=$OMP_DIR
-make all install
-echo "done with installing openMPI"
+make -j 2 all install
+echo "installed openMPI to $(pwd)"
