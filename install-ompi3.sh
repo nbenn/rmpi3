342ee63a165617626e957988910b483c14e95271
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-06-23 15:54:39 +0200

    re-enable sudo in travis

--- install-ompi3.sh
+++ install-ompi3.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -ex
+ls
+pwd
 mkdir openmpi3
 cd openmpi3
 wget --no-check-certificate https://download.open-mpi.org/release/open-mpi/v3.0/openmpi-3.0.0.tar.gz
 tar -zxf openmpi-3.0.0.tar.gz
 cd openmpi-3.0.0
 ./configure --prefix=/usr/openmpi3
-make all install
+sudo make all install
 echo "done with install"
 cd ..
 ls
